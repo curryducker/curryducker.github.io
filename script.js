55afa7d4ab8a91d4f391a248d75cc9e0f617f7df
curryducker <jur.hoekman@gmail.com>
2023-08-19 16:10:03 +0200

    tweaks

--- script.js
+++ script.js
@@ -8,14 +8,17 @@ if (!hasDarkModePreference) {
   document.body.classList.add('dark-mode');
   darkModeToggle.checked = true;
   localStorage.setItem('darkMode', true);
+  modeIcon.textContent = 'bedtime'; // Set moon icon
 } else {
   // Get the user's manual preference and apply it
   const userDarkModePreference = localStorage.getItem('darkMode') === 'true';
   document.body.classList.toggle('dark-mode', userDarkModePreference);
   darkModeToggle.checked = userDarkModePreference;
+  modeIcon.textContent = userDarkModePreference ? 'bedtime' : 'wb_sunny'; // Set icon based on preference
 }
 
 darkModeToggle.addEventListener('change', () => {
   document.body.classList.toggle('dark-mode', darkModeToggle.checked);
   localStorage.setItem('darkMode', darkModeToggle.checked);
+  modeIcon.textContent = darkModeToggle.checked ? 'bedtime' : 'wb_sunny'; // Update icon when toggling
 });
